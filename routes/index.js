13bce01df97c185dbb36ef7412b85df675270374
YangYgYang <you@example.com>
2022-10-29 10:44:33 +0800

    debug heroku

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
+const https = require("https")
 const express = require('express')
 const app = express()
 const router = express.Router()
 const home = require('./modules/home')
 const line = require('./modules/line')
 
-
+// router.use('/webhook', line)
 router.use('/', home)
-router.use('/webhook', line)
+
 
 
 
